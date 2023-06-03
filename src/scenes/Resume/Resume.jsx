6ff2c7cb4d4ca6e72215e286aee680db107d0555
Amarjeet <kumaramarjeetraj71@gmail.com>
2023-06-03 16:18:30 +0530

    first commit

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -11,7 +11,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const resumeLink =
-  'https://raw.githubusercontent.com/vsnaichuk/vsnaichuk/master/CV.pdf';
+  'https://github.com/rsap4you/Amarjeetcv/blob/main/Amarjeetcv.pdf';
 
 const Resume = () => {
   const pdfWrapper = useRef(null);
