b2da8b29e85075599f981bfb98941b2cfe9cc716
Amarjeet <kumaramarjeetraj71@gmail.com>
2023-06-03 15:47:20 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,69 @@
+import s from './Footer.module.scss';
+import { AiFillGithub } from 'react-icons/ai';
+import {
+  FaFacebookSquare,
+  FaLinkedinIn,
+  FaTelegramPlane,
+} from 'react-icons/fa';
+
+const Footer = () => {
+  // let date = new Date();
+  // let year = date.getFullYear();
+
+  return (
+    <div className={s.footer}>
+      <div className={s.container}>
+        <div className={s.copyright}>
+          <h1>Made with ❤️ by RSAP4UOU</h1>
+        </div>
+
+        <div className={s.body}>
+          <ul className={s.socialIcons}>
+            <li>
+              <a
+                href="https://github.com/rsap4you"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="github"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=100041391041692&mibextid=ZbWKwL"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="facebook"
+              >
+                <FaFacebookSquare />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://t.me/snaichuk_v"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="TelegramPlane"
+              >
+                <FaTelegramPlane />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/volodymyr-snaichuk-74a389124/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="linkedin"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/scenes/About/AboutTextCard/AboutTextCard.jsx
@@ -0,0 +1,39 @@
+import s from './AboutTextCard.module.scss';
+import { ImPointRight } from 'react-icons/im';
+
+const AboutTextCard = () => {
+  return (
+    <div className={s.card}>
+      <p style={{ textAlign: 'justify' }}>
+        Hi Everyone, I am{' '}
+        <span className={s.purple}>Amarjeet Kumar </span>
+        from <span className={s.purple}>Nalanda,Bihar(India)</span> Software engineer with expertise in cross-platform development
+        using Nodejs and Reactjs
+        <br />
+        <br />
+        I have a Bachelor's degree in Computer Science from  Nalanda College,Biharsharif Affilated with Magadh University(Bodh,Gaya).
+        <br />
+        Master's degree in Computer Science from Government Mca College,maninagar (Ahmedabad) affilated with Gujarat Technological University.
+        <br />
+        Apart from coding, some other activities that I love to do!
+      </p>
+
+      <ul>
+        <li className={s.aboutActivity}>
+          <ImPointRight /> Playing Games
+        </li>
+        <li className={s.aboutActivity}>
+          <ImPointRight /> Reading
+        </li>
+        <li className={s.aboutActivity}>
+          <ImPointRight /> Travelling
+        </li>
+        <li className={s.aboutActivity}>
+          <ImPointRight /> Listening Song
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AboutTextCard;
